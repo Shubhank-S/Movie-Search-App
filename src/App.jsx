0626a7e132c7827764e1f636fb98bae1aaed4225
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-15 19:16:17 +0530

    Setted the Routing set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
 export default App;
