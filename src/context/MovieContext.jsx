b8f8b44830f8a618ccbb7de1031371b30593d555
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-16 23:06:59 +0530

    Public API fetched

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,7 +1,23 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+
+const API_URL = `https://omdbapi.com/?apikey=47846924&s=titanic`;
 
 export const MovieContext = createContext();
 function MovieProvider({ children }) {
+  const [item, setitems] = useState([]);
+  const getMovies = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setitems(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <>
       <MovieContext.Provider value={"Shubhank" + "Sharma"}>
